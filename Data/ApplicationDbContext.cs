b42c4d9f20f5b671fff6007fd2f643e366425975
DilniThisaree <smdtjayathilake@students.nsbm.ac.lk>
2026-07-10 19:01:49 +0530

    done at7.01pm

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecruitmentPlatform.API.Models;
+
+namespace RecruitmentPlatform.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+
+                entity.HasIndex(u => u.Email)
+                      .IsUnique();
+
+                entity.Property(u => u.FullName)
+                      .HasMaxLength(100)
+                      .IsRequired();
+
+                entity.Property(u => u.Email)
+                      .HasMaxLength(255)
+                      .IsRequired();
+
+                entity.Property(u => u.PasswordHash)
+                      .HasMaxLength(255)
+                      .IsRequired();
+
+                entity.Property(u => u.Role)
+                      .HasMaxLength(30)
+                      .IsRequired();
+
+                entity.Property(u => u.IsActive)
+                      .HasDefaultValue(true);
+
+                entity.Property(u => u.CreatedAt)
+                      .IsRequired();
+            });
+        }
+    }
+}
